913a730f95a635b1c6de189ad55262227af0ab95
Mark <markporcarojr@gmail.com>
2025-02-24 16:07:56 -0500

    Update Email

--- components/BusinessCard.tsx
+++ components/BusinessCard.tsx
@@ -21,7 +21,7 @@ export default function BusinessCard() {
   const router = useRouter();
 
   const handleCopy = () => {
-    const text = "mibeerrox@gmail.com";
+    const text = "Studious.Alchemist13@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
     setTimeout(() => {
